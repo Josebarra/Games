08e7a753b3922143f42956f0bea0884525cd3a99
José Ignacio Barraquer Lopez <ji.barraquer@gmail.com>
2023-11-02 22:16:14 -0500

    add aliens and destroy  functionality. also ship hit and movement of the aliens

--- Alien_Invasion/settings.py
+++ Alien_Invasion/settings.py
@@ -10,10 +10,16 @@ class Settings:
 
         #ship settings
         self.ship_speed=4.5
+        self.ship_limit = 3
 
                 # Bullet settings
-        self.bullet_speed = 2.0
+        self.bullet_speed = 5.5
         self.bullet_width = 3
         self.bullet_height = 15
-        self.bullet_color = (60, 60, 60)
-        self.bullet_allowed = 3
+        self.bullet_color = (60, 0, 0)
+        self.bullet_allowed = 15
+        # Alien settings
+        self.alien_speed = 2.5
+        self.fleet_drop_speed = 25
+        # fleet_direction of 1 represents right; -1 represents left.
+        self.fleet_direction = 1
